97f6ef7796e36425f999a9a1026f970c27854ee2
alireza944 <alirezahajizade944@gmail.com>
2021-03-12 03:17:41 +0330

    arrayReference

---
+++ index.js
@@ -0,0 +1,23 @@
+let myArray =["ali", "ahmad" , "hossein" , "masoud"]
+let i=1;
+function myFunc(){
+    console.log(i);
+    i++;
+}
+myArray.forEach(myFunc)
+
+
+
+
+let my_Array=Array.from("alireza");
+console.log(my_Array)
+
+
+
+console.log(myArray.includes("hossein"))
+console.log(myArray.includes("Hossein"))
+console.log(myArray.indexOf("ahmad"))
+console.log(myArray.indexOf("Ahmad"))
+
+
+
